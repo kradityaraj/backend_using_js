354985b5ddbd68f7ed0a1cee4dbeca33981481bc
62057 <kr.adiraj0301@gmail.com>
2026-07-10 06:52:25 +0530

    Initial commit

---
+++ Setting up server/server.js
@@ -0,0 +1,34 @@
+// using http module (inbuilt given by node) we can setup a basic server
+
+const http = require('http');
+const PORT = 3000;
+
+/**
+* Http module contains a function called as createServer
+* this createServer function takes a callback as the input
+* Now inside the callback, we get two arguments
+* - request -> this argument contains all the details about the incoming request
+* - response -> this argument contains functions using which we can prepare the response
+*
+* the createServer function returns us the server object
+*/
+
+// We can used API client software to check the working of the server, like postman.
+const server = http.createServer(function exec(request, response) {
+    console.log(request.method);
+    if(request.url == '/home') {
+        response.end('Welcome to home');
+    }
+    else if(request.url == '/faq') {
+        response.end("Welcome to FAQs.")
+    }
+    else {
+        response.end('Hello World');
+    }
+});
+
+// PORT is used to diffentiate between the process.
+server.listen(PORT, function process() {
+    // once the server has started then this callback will be executed
+    console.log("Server started on port", PORT);
+});
